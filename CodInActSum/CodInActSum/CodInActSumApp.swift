a09940fbebc22206f269661e1d3a210bfaeafd83
Markylu <46211683+Markylu@users.noreply.github.com>
2023-05-15 12:32:10 +0800

    Created File

---
+++ CodInActSum/CodInActSum/CodInActSumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodInActSumApp.swift
+//  CodInActSum
+//
+//  Created by Markus Lu on 15/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodInActSumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
