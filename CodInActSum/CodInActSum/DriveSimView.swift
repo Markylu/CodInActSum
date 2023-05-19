5d796312d1926aacba54b77bc7570ed9017b8a62
AverageJJonak <declanssng@gmail.com>
2023-05-19 08:44:58 +0800

    drivesim

---
+++ CodInActSum/CodInActSum/DriveSimView.swift
@@ -0,0 +1,20 @@
+//
+//  DriveSimView.swift
+//  CodInActSum
+//
+//  Created by Declan Ng on 19/5/2023.
+//
+
+import SwiftUI
+
+struct DriveSimView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DriveSimView_Previews: PreviewProvider {
+    static var previews: some View {
+        DriveSimView()
+    }
+}
